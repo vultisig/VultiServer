26ab41c5b47e8ae81744839be065dacb99e214b8
0xkobe <dev_star11@outlook.com>
2024-07-12 00:15:54 +0800

    bug fix

--- config/config.go
+++ config/config.go
@@ -41,7 +41,8 @@ func init() {
 	viper.SetDefault("Redis.User", "")
 	viper.SetDefault("Redis.Password", "")
 	viper.SetDefault("Redis.DB", 0)
-	viper.SetDefault("Relay.Server", "http://localhost:8080")
+	viper.SetDefault("Relay.Server", "http://localhost:8080/router")
+	// viper.SetDefault("Relay.Server", "https://api.vultisig.com/router")
 
 	if err := viper.ReadInConfig(); err != nil {
 		log.Fatalf("Error reading config file, %s", err)
