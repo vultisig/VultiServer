407bb5c953efc93b83c8e7e0f314f2b4fe66ed0a
0xkobe <dev_star11@outlook.com>
2024-07-18 18:10:44 +0800

    add worker service

--- cmd/worker/main.go
+++ cmd/worker/main.go
@@ -1,23 +1,25 @@
 package main
 
 import (
-	"context"
-	"encoding/json"
-	"fmt"
 	"log"
 
 	"github.com/sirupsen/logrus"
 	"github.com/vultisig/vultisigner/config"
-	"github.com/vultisig/vultisigner/internal/keygen"
 	"github.com/vultisig/vultisigner/internal/logging"
 	"github.com/vultisig/vultisigner/internal/tasks"
-	"github.com/vultisig/vultisigner/internal/types"
-	"github.com/vultisig/vultisigner/storage"
+	"github.com/vultisig/vultisigner/service"
 
 	"github.com/hibiken/asynq"
 )
 
 func main() {
+	workerServce := &service.WorkerService{}
+	err := workerServce.Initialize()
+	if err != nil {
+		log.Fatalf("workerServce.Initialize failed: %v", err)
+		return
+	}
+
 	redisAddr := config.AppConfig.Redis.Host + ":" + config.AppConfig.Redis.Port
 
 	srv := asynq.NewServer(
@@ -38,75 +40,11 @@ func main() {
 
 	// mux maps a type to a handler
 	mux := asynq.NewServeMux()
-	mux.HandleFunc(tasks.TypeKeyGeneration, HandleKeyGeneration)
+	mux.HandleFunc(tasks.TypeKeyGeneration, workerServce.HandleKeyGeneration)
 	// mux.Handle(tasks.TypeKeyGeneration, tasks.I())
 	// ...register other handlers...
 
 	if err := srv.Run(mux); err != nil {
 		log.Fatalf("could not run server: %v", err)
 	}
-}
-
-type KeyGenerationTaskResult struct {
-	EDDSAPublicKey string
-	ECDSAPublicKey string
-}
-
-func HandleKeyGeneration(ctx context.Context, t *asynq.Task) error {
-	var p tasks.KeyGenerationPayload
-	if err := json.Unmarshal(t.Payload(), &p); err != nil {
-		return fmt.Errorf("json.Unmarshal failed: %v: %w", err, asynq.SkipRetry)
-	}
-	logging.Logger.WithFields(logrus.Fields{
-		"session":          p.SessionID,
-		"local_key":        p.LocalKey,
-		"chain_code":       p.ChainCode,
-		"HexEncryptionKey": p.HexEncryptionKey,
-	}).Info("Joining keygen")
-
-	keyECDSA, keyEDDSA, err := keygen.JoinKeyGeneration(&types.KeyGeneration{
-		Key:              p.LocalKey,
-		Session:          p.SessionID,
-		ChainCode:        p.ChainCode,
-		HexEncryptionKey: p.HexEncryptionKey,
-	})
-	if err != nil {
-		return fmt.Errorf("keygen.JoinKeyGeneration failed: %v: %w", err, asynq.SkipRetry)
-	}
-
-	logging.Logger.WithFields(logrus.Fields{
-		"keyECDSA": keyECDSA,
-		"keyEDDSA": keyEDDSA,
-	}).Info("Key generation completed")
-
-	redis, err := storage.NewRedisStorage(config.AppConfig)
-	if err != nil {
-		return fmt.Errorf("storage.NewRedisStorage failed: %v: %w", err, asynq.SkipRetry)
-	}
-
-	err = redis.RemoveVaultCacheItem(ctx, fmt.Sprintf("vault-%s-%s", p.Name, p.SessionID))
-	if err != nil {
-		logging.Logger.Errorf("redis.RemoveVaultCacheItem failed: %v", err)
-	}
-
-	err = redis.Close()
-	if err != nil {
-		logging.Logger.Errorf("redis.Close failed: %v", err)
-	}
-
-	result := KeyGenerationTaskResult{
-		EDDSAPublicKey: keyEDDSA,
-		ECDSAPublicKey: keyECDSA,
-	}
-
-	resultBytes, err := json.Marshal(result)
-	if err != nil {
-		return fmt.Errorf("json.Marshal failed: %v: %w", err, asynq.SkipRetry)
-	}
-
-	if _, err := t.ResultWriter().Write([]byte(resultBytes)); err != nil {
-		return fmt.Errorf("t.ResultWriter.Write failed: %v: %w", err, asynq.SkipRetry)
-	}
-
-	return nil
 }
---
+++ service/worker.go
@@ -0,0 +1,83 @@
+package service
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/hibiken/asynq"
+	"github.com/sirupsen/logrus"
+	"github.com/vultisig/vultisigner/config"
+	"github.com/vultisig/vultisigner/internal/keygen"
+	"github.com/vultisig/vultisigner/internal/logging"
+	"github.com/vultisig/vultisigner/internal/tasks"
+	"github.com/vultisig/vultisigner/internal/types"
+	"github.com/vultisig/vultisigner/storage"
+)
+
+type WorkerService struct {
+	redis *storage.RedisStorage
+}
+
+func (s *WorkerService) Initialize() error {
+	redis, err := storage.NewRedisStorage(config.AppConfig)
+	if err != nil {
+		return fmt.Errorf("storage.NewRedisStorage failed: %w", err)
+	}
+	s.redis = redis
+	return nil
+}
+
+type KeyGenerationTaskResult struct {
+	EDDSAPublicKey string
+	ECDSAPublicKey string
+}
+
+func (s *WorkerService) HandleKeyGeneration(ctx context.Context, t *asynq.Task) error {
+	var p tasks.KeyGenerationPayload
+	if err := json.Unmarshal(t.Payload(), &p); err != nil {
+		return fmt.Errorf("json.Unmarshal failed: %v: %w", err, asynq.SkipRetry)
+	}
+	logging.Logger.WithFields(logrus.Fields{
+		"session":          p.SessionID,
+		"local_key":        p.LocalKey,
+		"chain_code":       p.ChainCode,
+		"HexEncryptionKey": p.HexEncryptionKey,
+	}).Info("Joining keygen")
+
+	keyECDSA, keyEDDSA, err := keygen.JoinKeyGeneration(&types.KeyGeneration{
+		Key:              p.LocalKey,
+		Session:          p.SessionID,
+		ChainCode:        p.ChainCode,
+		HexEncryptionKey: p.HexEncryptionKey,
+	})
+	if err != nil {
+		return fmt.Errorf("keygen.JoinKeyGeneration failed: %v: %w", err, asynq.SkipRetry)
+	}
+
+	logging.Logger.WithFields(logrus.Fields{
+		"keyECDSA": keyECDSA,
+		"keyEDDSA": keyEDDSA,
+	}).Info("Key generation completed")
+
+	err = s.redis.RemoveVaultCacheItem(ctx, fmt.Sprintf("vault-%s-%s", p.Name, p.SessionID))
+	if err != nil {
+		logging.Logger.Errorf("redis.RemoveVaultCacheItem failed: %v", err)
+	}
+
+	result := KeyGenerationTaskResult{
+		EDDSAPublicKey: keyEDDSA,
+		ECDSAPublicKey: keyECDSA,
+	}
+
+	resultBytes, err := json.Marshal(result)
+	if err != nil {
+		return fmt.Errorf("json.Marshal failed: %v: %w", err, asynq.SkipRetry)
+	}
+
+	if _, err := t.ResultWriter().Write([]byte(resultBytes)); err != nil {
+		return fmt.Errorf("t.ResultWriter.Write failed: %v: %w", err, asynq.SkipRetry)
+	}
+
+	return nil
+}
