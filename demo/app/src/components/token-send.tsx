3d0c7812ca816a4da291150d2eb43368d11e7593
0xkobe <dev_star11@outlook.com>
2024-07-26 21:45:22 +0800

    wip: update token-send component

--- demo/app/src/components/token-send.tsx
+++ demo/app/src/components/token-send.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { initWasm, TW } from "@trustwallet/wallet-core";
+import { initWasm, TW, WalletCore } from "@trustwallet/wallet-core";
 
 const TokenSend: React.FC = () => {
   const [vaultAddress, setVaultAddress] = useState<string>("");
@@ -24,6 +24,162 @@ const TokenSend: React.FC = () => {
     }
   };
 
+  function getPreSignedInputData(
+    walletCore: WalletCore,
+    keysignPayload: KeysignPayload
+  ): Uint8Array {
+    const { AnyAddress } = walletCore;
+
+    const fromAddr = AnyAddress.createWithString(
+      keysignPayload.coin.address,
+      walletCore.CoinType.thorchain
+    );
+    if (!fromAddr) {
+      console.error(`${keysignPayload.coin.address} is invalid`);
+      return Uint8Array.of();
+    }
+
+    if (!keysignPayload.chainSpecific) {
+      console.error("fail to get account number, sequence, or fee");
+      return Uint8Array.of();
+    }
+
+    const { accountNumber, sequence } = keysignPayload.chainSpecific;
+    const pubKeyData = Buffer.from(keysignPayload.coin.hexPublicKey, "hex");
+    if (!pubKeyData) {
+      console.error("invalid hex public key");
+      return Uint8Array.of();
+    }
+
+    const toAddress = AnyAddress.createWithString(
+      keysignPayload.toAddress,
+      walletCore.CoinType.thorchain
+    );
+    if (!toAddress) {
+      console.error(`${keysignPayload.toAddress} is invalid`);
+      return Uint8Array.of();
+    }
+
+    const message = [
+      TW.Cosmos.Proto.Message.create({
+        thorchainSendMessage: TW.Cosmos.Proto.Message.THORChainSend.create({
+          fromAddress: fromAddr.data(),
+          amounts: [
+            TW.Cosmos.Proto.Amount.create({
+              denom: "rune",
+              amount: keysignPayload.toAmount.toString(),
+            }),
+          ],
+          toAddress: toAddress.data(),
+        }),
+      }),
+    ];
+
+    let chainId = walletCore.CoinTypeExt.chainId(walletCore.CoinType.thorchain);
+    // if (chainId !== walletCore.Blockchain.thorchain.value.toString()) {
+    //   chainId = walletCore.Blockchain.thorchain.value.toString();
+    // }
+
+    const input = TW.Cosmos.Proto.SigningInput.create({
+      publicKey: pubKeyData,
+      signingMode: TW.Cosmos.Proto.SigningMode.Protobuf,
+      chainId: chainId,
+      accountNumber: accountNumber,
+      sequence: sequence,
+      mode: TW.Cosmos.Proto.BroadcastMode.SYNC,
+      memo: keysignPayload.memo,
+      messages: message,
+      fee: { gas: 20000000 },
+    });
+
+    return TW.Cosmos.Proto.SigningInput.encode(input).finish();
+  }
+
+  function getSignedTransaction(
+    walletCore: WalletCore,
+    vaultHexPubKey: string,
+    vaultHexChainCode: string,
+    keysignPayload: KeysignPayload,
+    signatures: { [key: string]: TssKeysignResponse }
+  ) {
+    const inputData = getPreSignedInputData(walletCore, keysignPayload);
+    const thorPublicKey = getDerivedPubKey(
+      vaultHexPubKey,
+      vaultHexChainCode,
+      walletCore.CoinTypeExt.derivationPath(walletCore.CoinType.thorchain)
+    ); // need to get it from tss service
+    const pubkeyData = Buffer.from(thorPublicKey, "hex");
+    const publicKey = walletCore.PublicKey.createWithData(
+      pubkeyData,
+      walletCore.PublicKeyType.secp256k1
+    );
+
+    if (!publicKey) {
+      console.error(`public key ${thorPublicKey} is invalid`);
+      return;
+    }
+
+    try {
+      const hashes = walletCore.TransactionCompiler.preImageHashes(
+        walletCore.CoinType.thorchain,
+        inputData
+      );
+      const preSigningOutput =
+        TW.TxCompiler.Proto.PreSigningOutput.decode(hashes);
+      const allSignatures = new walletCore.DataVector();
+      const publicKeys = new walletCore.DataVector();
+      const signature = signatures[preSigningOutput.dataHash.toString()];
+
+      if (!publicKey.verify(signature, preSigningOutput.dataHash)) {
+        console.error("fail to verify signature");
+        return;
+      }
+
+      allSignatures.add(signature);
+      publicKeys.add(pubkeyData);
+
+      const compileWithSignature =
+        walletCore.TransactionCompiler.compileWithSignatures(
+          walletCore.CoinType.thorchain,
+          inputData,
+          allSignatures,
+          publicKeys
+        );
+      const output = TW.Cosmos.Proto.SigningOutput.decode(compileWithSignature);
+      return output.serialized; // raw transaction
+    } catch (error: any) {
+      console.error(
+        `fail to get signed ethereum transaction, error: ${error.message}`
+      );
+      return;
+    }
+  }
+
+  async function broadcastSignedTransaction(signedTx: string): Promise<any> {
+    try {
+      const response = await fetch(`${rpcEndpoint}/broadcast`, {
+        method: "POST",
+        body: signedTx,
+      });
+
+      if (response.status === 200) {
+        const data = await response.json();
+        console.log("Transaction broadcasted successfully:", data);
+        return data;
+      } else {
+        console.error(
+          "Failed to broadcast transaction:",
+          response.status,
+          response.statusText
+        );
+        return;
+      }
+    } catch (error) {
+      console.error("Error broadcasting transaction:", error);
+      return;
+    }
+  }
+
   const sendTransaction = async () => {
     if (!vaultAddress || !toAddress || !amount) return;
 
@@ -35,41 +191,14 @@ const TokenSend: React.FC = () => {
       const accountNumber = accountInfo.data.result.value.account_number;
       const sequence = accountInfo.data.result.value.sequence;
 
-      //   const tx = TW.THORChainSwap.Proto.SwapInput.create({
-      //     fromAddress: vaultAddress,
-      //     toAddress: toAddress,
-      //     fromAmount: amount,
-      //     // fromAsset: 'rune',
-      //     // memo: '',
-      //   });
-      //   const tx = create({
-      //     accountNumber: accountNumber,
-      //     chainId: "thorchain",
-      //     fee: {
-      //       amounts: [{ denom: "rune", amount: "2000" }],
-      //       gas: "200000",
-      //     },
-      //     memo: "",
-      //     msgs: [
-      //       {
-      //         type: "cosmos-sdk/MsgSend",
-      //         value: {
-      //           from_address: vaultAddress,
-      //           to_address: toAddress,
-      //           amount: [{ denom: "rune", amount: amount }],
-      //         },
-      //       },
-      //     ],
-      //     sequence: sequence,
-      //   });
+      // build keysign request
+      // api call to get keysignPayload, chain code
+      // api call to get signatures after task completed
 
-      //   const encodedTx = tx.encode();
-      //   const unsignedTx = Buffer.from(encodedTx).toString("hex");
-
-      // api call
-
-      //   const response = await provider.sendTransaction(signedTx);
-      //   setTransactionHash(response.hash);
+      //   const signedTx = getSignedTransaction(walletCore, vaultAddress, vaultHexChainCode, keysignPayload, signatures)
+      //   if (signedTx) {
+      //     broadcastSignedTransaction(signedTx)
+      //   }
     } catch (error) {
       console.error("Error sending transaction:", error);
     }
